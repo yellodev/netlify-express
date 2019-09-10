d718847f8249fb83950bd5d298087174e3830fc2
Reef <dev@yello.re>
2019-09-10 19:20:26 +0400

    test

--- express/server.js
+++ express/server.js
@@ -4,26 +4,17 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-/*
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>r33f API server!</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda */
-
-app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-app.listen(8000, () => console.log('Local app listening on port 8000!'));
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
